fa9e681dacb59ea86218816bc8de384a4cd97023
jraines <johndanielraines@gmail.com>
2020-12-05 13:47:20 -0600

    Day05

---
+++ AOC2020/DayLibs/Day05Lib/Seat.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AOC2020.DayLibs.Day05Lib
+{
+    public class Seat
+    {
+
+        public Seat(string raw)
+        {
+            Raw = raw;
+            binRow = ToBinary(raw[0..7]);
+            binColumn = ToBinary(raw[7..]);
+            Row = Convert.ToInt32(binRow, 2);
+            Column = Convert.ToInt32(binColumn, 2);
+            ID = (Row * 8) + Column;
+        }
+
+        public override string ToString()
+        {
+            return $"<Seat::\n" +
+                   $"    Raw={Raw}\n" +
+                   $"    Row={Row}, binRow={binRow}\n" +
+                   $"    Column={Column}, binColumn={binColumn}\n" +
+                   $"    ID={ID}>";
+        }
+
+        private Dictionary<char, char> binConv = new Dictionary<char, char>()
+            {
+                {'F', '0'}, {'B','1'}, {'L', '0'}, {'R', '1'}
+            };
+
+        private string Raw;
+        private string binRow;
+        private string binColumn;
+        public int Row;
+        public int Column;
+        public int ID;
+
+        private string ToBinary(string seatRaw)
+        {
+            char[] seatBin = new char[seatRaw.Length];
+            for (int i = 0; i < seatRaw.Length; i++)
+            {
+                seatBin[i] = binConv[seatRaw[i]];
+            }
+            return new string(seatBin);
+        }
+
+    }
+}
---
+++ AOC2020/DayPuzzles/Day05Puzzle.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using AOC2020.AOCInput;
+using AOC2020.DayLibs.Day05Lib;
+
+
+namespace AOC2020.DayPuzzles
+{
+    public class Day05Puzzle : IDayPuzzle
+    {
+        public Day05Puzzle()
+        {
+        }
+
+        public string Day {get;} = "05";
+
+        public void SolvePart1()
+        {
+            string[] input = InputParser.ReadToStringArr(Day, "1");
+            Seat[] seats = new Seat[input.Length];
+
+            for(int i=0; i < input.Length; i++)
+            {
+                seats[i] = new Seat(input[i]);
+            };
+
+            int maxSeatID = 0;
+            foreach (Seat seat in seats)
+            {
+                if (seat.ID > maxSeatID)
+                {
+                    maxSeatID = seat.ID;
+                }
+            }
+            Console.WriteLine($"Highest seat ID: {maxSeatID}");
+        }
+
+        public void SolvePart2()
+        {
+            string[] input = InputParser.ReadToStringArr(Day, "1");
+            Seat[] seats = new Seat[input.Length];
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                seats[i] = new Seat(input[i]);
+            };
+
+            Seat[] sorted = seats.OrderBy(s => s.ID).ToArray();
+            List<int> missingSeat = new List<int>();
+            for(int i=0; i < sorted.Length - 1; i++)
+            {
+                if (sorted[i + 1].ID - 1 == sorted[i].ID + 1)
+                {
+                    Console.WriteLine($"Missing seat: {sorted[i].ID + 1}");
+                }
+            }
+        }
+
+        public void SolveTest()
+        {
+            string[] input = InputParser.ReadToStringArr(Day, "Test");
+            Seat[] seats = new Seat[input.Length];
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                seats[i] = new Seat(input[i]);
+            };
+
+            int maxSeatID = 0;
+            foreach (Seat seat in seats)
+            {
+                Console.WriteLine(seat.ToString());
+
+                if (seat.ID > maxSeatID)
+                {
+                    maxSeatID = seat.ID;
+                }
+            }
+        }
+    }
+}
