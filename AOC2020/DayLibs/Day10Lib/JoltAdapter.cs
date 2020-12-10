793cac6effa57d799161f821da1d7fabafe755c7
jraines <johndanielraines@gmail.com>
2020-12-10 09:16:04 -0600

    Day 10 Part 1

---
+++ AOC2020/DayLibs/Day10Lib/JoltAdapter.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+namespace AOC2020.DayLibs.Day10Lib
+{
+
+    public interface IPowerRated
+    {
+        public int Rating { get; set; }
+        public int inDiff { get; }
+    }
+
+    public interface IPowerSource : IPowerRated
+    {
+        public IPowerRecipient outConn { get; set; }
+    }
+
+    public interface IPowerRecipient : IPowerRated
+    {
+        public IPowerSource inConn { get; set; }
+        public void ConnectSource(IPowerSource src);
+    }
+
+    public class WallOutlet : IPowerRated, IPowerSource
+    {
+        public int Rating { get; set; }
+        public IPowerRecipient outConn { get; set; }
+        public int inDiff { get; } = 0;
+        public WallOutlet(int rating=0)
+        {
+            Rating = rating;
+        }
+    }
+
+    public class Device : IPowerRecipient
+    {
+        public IPowerSource inConn { get; set; }
+        public int Rating { get; set; }
+        public int inDiff => this.Rating - inConn.Rating;
+
+        public Device(int rating)
+        {
+            Rating = rating;
+        }
+
+        public void ConnectSource(IPowerSource src)
+        {
+            inConn = src;
+            src.outConn = this;
+        }
+    }
+
+    public class JoltAdapter : IPowerSource, IPowerRecipient
+    {
+        public int Rating { get; set; }
+        public IPowerSource inConn { get; set; } = new WallOutlet();
+        public IPowerRecipient outConn { get; set; } = new Device(0);
+        public int inDiff
+        {
+            get
+            {
+                return this.Rating - inConn.Rating;
+            }
+        }
+
+        public JoltAdapter(int rating)
+        {
+            Rating = rating;
+        }
+
+        public void ConnectSource(IPowerSource src)
+        {
+            if(src.Rating > this.Rating)
+            {
+                throw new ArgumentException($"The adapter being connected has a " +
+                                            $"higher rating than this adapter: { src.Rating } > { Rating }");
+            }
+            inConn = src;
+            src.outConn = this;
+        }
+
+        public void ConnectRecipient(IPowerRecipient device)
+
+        {
+            outConn = device;
+            device.inConn = this;
+        }
+
+        public override string ToString()
+        {
+            return $"<JoltAdapter:: Rating={Rating}, inDiff={inDiff}>";
+        }
+    }
+}
--- AOC2020/DayPuzzles/Day10Puzzle.cs
+++ AOC2020/DayPuzzles/Day10Puzzle.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 using AOC2020.AOCInput;
+using AOC2020.DayLibs.Day10Lib;
 
 namespace AOC2020.DayPuzzles
 {
@@ -9,17 +12,56 @@ namespace AOC2020.DayPuzzles
 
         public void SolvePart1()
         {
-            throw new NotImplementedException();
+            int[] adapterRatings = InputParser.ReadToIntArr(Day, "1");
+            JoltAdapter[] adapters = new JoltAdapter[adapterRatings.Length];
+            for(int i=0; i < adapterRatings.Length; i++)
+            {
+                adapters[i] = new JoltAdapter(adapterRatings[i]);
+            }
+            Device device = new Device(adapters.Select(a => a.Rating).Max() + 3);
+            JoltAdapter[] chain = CreateSimpleChain(adapters);
+            chain[^1].ConnectRecipient(device);
+            int deviceDiff = device.inDiff;
+
+            int oneDiffs = chain.Where(a => a.inDiff == 1).Count();
+            int threeDiffs = chain.Where(a => a.inDiff == 3).Count();
+
+            if (deviceDiff == 1) { oneDiffs++; }
+            else if (deviceDiff == 3) { threeDiffs++; }
+            Console.WriteLine($"Solution: {oneDiffs * threeDiffs}");
         }
 
         public void SolvePart2()
-        {
-            throw new NotImplementedException();
-        }
+        { }
 
         public void SolveTest()
+        { }
+
+        private JoltAdapter[] CreateSimpleChain(JoltAdapter[] adapterArr)
         {
-            throw new NotImplementedException();
+            JoltAdapter[] adapters = adapterArr.ToList().OrderBy(a=>a.Rating).ToArray();
+
+            for (int i = 1; i < adapterArr.Length; i++)
+            {
+                if (i == 0)
+                { }
+                else if (CanConnect(adapters[i-1],adapters[i]))
+                {
+                    adapters[i].ConnectSource(adapters[i - 1]);
+                }
+                else
+                {
+                    throw new Exception($"Cannot connect thes two consecutive adapters rated {adapters[i - 1].Rating} & {adapters[i].Rating}");
+                }
+            }
+
+            return adapters;
+        }
+
+        private bool CanConnect(IPowerRated a, IPowerRated b)
+        {
+            int diff = Math.Abs(b.Rating - a.Rating);
+            return diff < 3 && diff >= 0;
         }
     }
 }
