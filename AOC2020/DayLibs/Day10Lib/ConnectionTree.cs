5ea841a1cf4c36c1f778e301489f17bc8456191a
John Raines <john_raines@choosebroadspire.com>
2020-12-10 13:35:34 -0600

    Toward a D10P2 solution

---
+++ AOC2020/DayLibs/Day10Lib/ConnectionTree.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AOC2020.DayLibs.Day10Lib
+{
+    public static class VirtualConnectionTree
+    {
+
+        public static long TreeNodeCount(JoltAdapter[] adapters)
+        {
+            adapters = Utilities.SortAdapters(adapters);
+            int[] ratings = new int[adapters.Length + 2];
+            ratings[0] = 0;
+            ratings[^1] = Utilities.GetDeviceRating(adapters);
+
+
+            using AdapterChoices = Dictionary<int, List<int>>;
+
+            AdapterChoices choices = new AdapterChoices();
+
+            for (int i = 0; i < adapters.Length; i++)
+            {
+                // create an AdapterChoices dictionary using a function that creates a list of choices, given some rating
+            }
+        }
+
+        public static List<int> GetChoices(int rating, JoltAdapter[] adapters)
+        {
+            // generate a List<int> of ratings choices.
+        }
+
+    }
+}
---
+++ AOC2020/DayLibs/Day10Lib/Utilities.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AOC2020.DayLibs.Day10Lib
+{
+    public static class Utilities
+    {
+
+        public static JoltAdapter[] SortAdapters(JoltAdapter[] adapters)
+        {
+            return adapters.ToList().OrderBy(a => a.Rating).ToArray();
+        }
+
+        public static int GetDeviceRating(JoltAdapter[] adapters)
+        {
+            return adapters.Select(a => a.Rating).Max() + 3;
+        }
+
+        public static JoltAdapter[] CreateSimpleChain(JoltAdapter[] adapterArr)
+        {
+            JoltAdapter[] adapters = SortAdapters(adapterArr);
+
+            for (int i = 1; i < adapterArr.Length; i++)
+            {
+                if (i == 0)
+                { }
+                else if (CanConnect(adapters[i - 1], adapters[i]))
+                {
+                    adapters[i].ConnectSource(adapters[i - 1]);
+                }
+                else
+                {
+                    throw new Exception($"Cannot connect thes two consecutive adapters rated {adapters[i - 1].Rating} & {adapters[i].Rating}");
+                }
+            }
+
+            return adapters;
+        }
+        public static bool CanConnect(IPowerRated a, IPowerRated b)
+        {
+            int diff = Math.Abs(b.Rating - a.Rating);
+            return diff < 3 && diff >= 0;
+        }
+    }
+}
