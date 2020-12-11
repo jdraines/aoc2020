aa85f4672764de3ccf23148526c5c83e13b25861
jraines <johndanielraines@gmail.com>
2020-12-11 14:20:43 -0600

    2dChar parser in day11.

--- AOC2020/DayLibs/Day11Lib/Utilities.cs
+++ AOC2020/DayLibs/Day11Lib/Utilities.cs
@@ -9,6 +9,16 @@ namespace AOC2020.DayLibs.Day11Lib
         public static char[,] ReadTo2dCharArr(string day, string part)
         {
             string[] input = InputParser.ReadToStringArr(day, part);
+            char[,] seatMap = new char[input.Length, input[0].Length];
+            for (int y = 0; y < input.Length; y++)
+            {
+                for (int x = 0; x < input[0].Length; x++)
+                { 
+                    seatMap[y, x] = input[y][x];
+                }
+            }
+
+            return seatMap;
         }
 
     }
