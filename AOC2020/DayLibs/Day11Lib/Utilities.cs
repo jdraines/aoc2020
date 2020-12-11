ad4e7dbf1a3ce6f069dfb2c039128f9e18f3b44a
jraines <johndanielraines@gmail.com>
2020-12-11 13:00:13 -0600

    Day 11 input parsing

--- AOC2020/DayLibs/Day11Lib/Utilities.cs
+++ AOC2020/DayLibs/Day11Lib/Utilities.cs
@@ -1,8 +1,15 @@
 ﻿using System;
+using AOC2020.AOCInput;
+
 namespace AOC2020.DayLibs.Day11Lib
 {
     public static class Utilities
     {
-        
+
+        public static char[,] ReadTo2dCharArr(string day, string part)
+        {
+            string[] input = InputParser.ReadToStringArr(day, part);
+        }
+
     }
 }
--- AOC2020/DayPuzzles/Day11Puzzle.cs
+++ AOC2020/DayPuzzles/Day11Puzzle.cs
@@ -1,10 +1,25 @@
 ﻿using System;
+using AOC2020.DayLibs.Day11Lib;
+
 namespace AOC2020.DayPuzzles
 {
-    public class Day11Puzzle
+    public class Day11Puzzle : IDayPuzzle
     {
-        public Day11Puzzle()
+        public string Day { get; } = "11";
+
+        public void SolvePart1()
         {
+            char[,] seatMap = Utilities.ReadTo2dCharArr(Day, "1");
+        }
+
+        public void SolvePart2()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SolveTest()
+        {
+            throw new NotImplementedException();
         }
     }
 }
