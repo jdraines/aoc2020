b3245abac0a7f9fb6adad963b0786b1d35e99cf6
jraines <johndanielraines@gmail.com>
2020-12-15 10:42:02 -0600

    Day 15 setup

---
+++ AOC2020/DayPuzzles/Day15Puzzle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace AOC2020.DayPuzzles
+{
+    public class Day15Puzzle : IDayPuzzle
+    {
+        public string Day { get; } = "15";
+
+        public void SolvePart1()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SolvePart2()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SolveTest()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
