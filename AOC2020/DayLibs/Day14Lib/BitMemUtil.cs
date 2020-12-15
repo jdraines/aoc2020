81b319263247755454df30092fa0ebe98bb6786c
jraines <johndanielraines@gmail.com>
2020-12-15 10:39:38 -0600

    Day 14

---
+++ AOC2020/DayLibs/Day14Lib/BitMemUtil.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace AOC2020.DayLibs.Day14Lib
+{
+    public static class BitMemUtil
+    {
+
+
+        public static string GetMask(string line)
+        {
+            return line.Split(" = ")[1];
+        }
+
+        public static string ToBit36(string intString)
+        {
+            return Convert.ToString(Int32.Parse(intString), 2).PadLeft(36, '0');
+        }
+
+        public static string ToBit36(long longInt)
+        {
+            return Convert.ToString(longInt, 2).PadLeft(36, '0');
+        }
+
+        public static string ToBit36(int int32)
+        {
+            return Convert.ToString(int32, 2).PadLeft(36, '0');
+        }
+
+
+
+        public static Tuple<long, string> ParseMemSet(string line)
+        {
+            string[] parts = line.Split(" = ");
+            long keyLong = Int64.Parse(parts[0].Split('[')[1].Split(']')[0]);
+            string bitStr = ToBit36(parts[1]);
+            return new Tuple<long, string>(keyLong, bitStr);
+        }
+
+        public static Tuple<List<string>, long> ParseMemSet(string line, string mask)
+        {
+            string[] parts = line.Split(" = ");
+            long keyLong = Int64.Parse(parts[0].Split('[')[1].Split(']')[0]);
+            long val = Int64.Parse(parts[1]);
+            string masked = ApplyMaskP2(ToBit36(keyLong), mask);
+            return new Tuple<List<string>, long>(GenFloatPermutations(masked), val);
+        }
+
+        public static string ApplyMaskP1(string int36, string mask)
+        {
+            char[] masked = new char[36];
+
+            for (int i = 0; i < 36; i++)
+            {
+                if (mask[i] == 'X')
+                {
+                    masked[i] = int36[i];
+                }
+
+                else if (mask[i] == '1')
+                {
+                    masked[i] = '1';
+                }
+                else if (mask[i] == '0')
+                {
+                    masked[i] = '0';
+                }
+            }
+            return new string(masked);
+        }
+
+        public static string ApplyMaskP2(string int36, string mask)
+        {
+            char[] masked = new char[36];
+            for (int i = 0; i < 36; i++)
+            {
+                if (mask[i] == 'X')
+                {
+                    masked[i] = 'X';
+                }
+
+                else if (mask[i] == '1')
+                {
+                    masked[i] = '1';
+                }
+                else if (mask[i] == '0')
+                {
+                    masked[i] = int36[i];
+                }
+            }
+            return new string(masked);
+        }
+
+
+        public static List<string> GenFloatPermutations(string bitStr)
+        {
+            char[] newStr;
+            List<string> permutations = new List<string>();
+            var indices = new List<int>();
+
+            for (int i = 0; i < bitStr.Length; i++)
+            {
+                if (bitStr[i] == 'X')
+                {
+                    indices.Add(i);
+                }
+            }
+
+            Console.WriteLine($"indices: {String.Join(", ", indices.Select(xi => xi.ToString()))}\nbitStr: {bitStr}");
+
+            if (indices.Count == 0)
+            {
+                return new List<string>() { bitStr };
+            }
+
+            List<List<bool>> boolPerms = GenPermutations(bitStr, indices.Count, new List<List<bool>>());
+
+            for (int i = 0; i < boolPerms.Count; i++)
+            {
+                newStr = bitStr.ToArray();
+                for (int j = 0; j < indices.Count; j++)
+                {
+                    if (boolPerms[i][j])
+                    {
+                        newStr[indices[j]] = '1';
+                    }
+                    else
+                    {
+                        newStr[indices[j]] = '0';
+                    }
+                }
+                permutations.Add(new string(newStr));
+            }
+            return permutations;
+        }
+
+        public static List<List<bool>> GenPermutations(string bitStr, int length, List<List<bool>> memo)
+        {
+            var _master = new List<List<bool>>();
+            var _memo = new List<List<bool>>();
+            memo = memo.Select(x => x).ToList();
+
+            if (length == 1)
+            {
+                foreach (bool val in new bool[] { true, false })
+                {
+                    memo.Add(new List<bool> { val });
+                }
+
+                return memo;
+            }
+            else
+            {
+                foreach (bool val in new bool[] { true, false })
+                {
+                    _memo = GenPermutations(bitStr, length - 1, memo);
+
+                    foreach (var perm in _memo)
+                    {
+                        perm.Add(val);
+                    }
+                    _master.AddRange(_memo);
+                }
+                return _master;
+            }
+        }
+
+
+    }
+}
---
+++ AOC2020/DayPuzzles/Day14Puzzle.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using AOC2020.AOCInput;
+using Util = AOC2020.DayLibs.Day14Lib.BitMemUtil;
+
+namespace AOC2020.DayPuzzles 
+{
+    public class Day14Puzzle : IDayPuzzle
+    {
+
+        public string Day { get; } = "14";
+
+        public void SolvePart1()
+        {
+            string[] lines = InputParser.ReadToStringArr(Day);
+            var memory = new Dictionary<long, string>();
+            string mask = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+            long memLoc;
+            string bitStr;
+            long solution = 0;
+
+            foreach (string line in lines)
+            {
+                if(line.StartsWith("mask"))
+                {
+                    mask = Util.GetMask(line);
+                }
+                else if (line.StartsWith("mem"))
+                {
+                    (memLoc, bitStr) = Util.ParseMemSet(line);
+                    if (memory.ContainsKey(memLoc))
+                    {
+                        memory[memLoc] = Util.ApplyMaskP1(bitStr, mask);
+                    }
+                    else
+                    {
+                        memory.Add(memLoc, Util.ApplyMaskP1(bitStr, mask));
+                    }
+                }
+            }
+
+            foreach (KeyValuePair<long, string> entry in memory)
+            {
+                try
+                {
+                    solution += Convert.ToInt64(entry.Value.Trim(), 2);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(entry.Value);
+                    throw e;
+                }
+                
+            }
+            Console.WriteLine($"Solution: {solution}");
+        }
+
+        public void SolvePart2()
+        {
+            string[] lines = InputParser.ReadToStringArr(Day);
+            var memory = new Dictionary<long, long>();
+            string mask = "000000000000000000000000000000000000";
+            List<string> memLocs;
+            long val;
+            long solution = 0;
+
+            foreach (string line in lines)
+            {
+                if (line.StartsWith("mask"))
+                {
+                    mask = Util.GetMask(line);
+                }
+                else if (line.StartsWith("mem"))
+                {
+                    (memLocs, val) = Util.ParseMemSet(line, mask);
+                    foreach(string memLoc in memLocs)
+                    {
+                        long loc = Convert.ToInt64(memLoc, 2);
+
+                        if (memory.ContainsKey(loc))
+                        {
+                            memory[loc] = val;
+                        }
+                        else
+                        {
+                            memory.Add(loc, val);
+                        }
+                    }
+
+                }
+            }
+
+            foreach (KeyValuePair<long, long> entry in memory)
+            {
+                solution += entry.Value;
+            }
+            Console.WriteLine($"Solution: {solution}");
+        }
+
+        public void SolveTest()
+        {
+            string[] lines = InputParser.ReadToStringArr(Day, "Test");
+            var memory = new Dictionary<long, long>();
+            string mask = "000000000000000000000000000000000000";
+            List<string> memLocs;
+            long val;
+            long solution = 0;
+
+            foreach (string line in lines)
+            {
+                if (line.StartsWith("mask"))
+                {
+                    mask = Util.GetMask(line);
+                }
+                else if (line.StartsWith("mem"))
+                {
+                    (memLocs, val) = Util.ParseMemSet(line, mask);
+                    foreach (string memLoc in memLocs)
+                    {
+                        long loc = Convert.ToInt64(memLoc, 2);
+
+                        if (memory.ContainsKey(loc))
+                        {
+                            memory[loc] = val;
+                        }
+                        else
+                        {
+                            memory.Add(loc, val);
+                        }
+                    }
+
+                }
+            }
+
+            foreach (KeyValuePair<long, long> entry in memory)
+            {
+                Console.WriteLine($"{entry.Key}={entry.Value}");
+                solution += entry.Value;
+            }
+            Console.WriteLine($"Solution: {solution}");
+        }
+    }
+}
