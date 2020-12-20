e8d6447e3cbcab259fe4680adc9fcee543d8dbe5
jraines <johndanielraines@gmail.com>
2020-12-20 13:56:21 -0600

    Day 19 P1

---
+++ AOC2020/DayLibs/Day19Lib/D19Util.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using AOC2020.AOCInput;
+
+namespace AOC2020.DayLibs.Day19Lib
+{
+    public static class D19Util
+    {
+
+        public static string[] GetMessages(string day="19", string part="1")
+        {
+            string messagesRaw = InputParser.ReadChunksToStringArr(day, part)[1];
+            return messagesRaw.Split('\n');
+        }
+
+        public static Dictionary<int, string> GetRules(string day="19", string part="1")
+        {
+            Dictionary<int, string> rules = new Dictionary<int, string>();
+            string rulesRaw = InputParser.ReadChunksToStringArr(day, part)[0];
+            string[] ruleSplit;
+
+            foreach (string rule in rulesRaw.Split('\n'))
+            {
+                if (String.IsNullOrEmpty(rule) || String.IsNullOrWhiteSpace(rule))
+                { continue;  }
+
+                ruleSplit = rule.Split(':');
+                try
+                {
+                    rules.Add(Int32.Parse(ruleSplit[0].Trim()), ruleSplit[1].Trim());
+                }
+                catch
+                {
+                    Console.WriteLine($"'{rule}'");
+                    throw;
+                }
+            }
+
+            return rules;
+        }
+
+        public static string GenRegexRule(int ruleNo, Dictionary<int, string> rules)
+        {
+            string rule = rules[ruleNo];
+            string regexRule = "(";
+            string[] rSeq = rule.Split('|');
+
+            if (rule.StartsWith("\""))
+            {
+                return GetRuleVal(rule);
+            }
+
+            for (int i = 0; i < rSeq.Length; i++)
+            {
+                foreach(string rNo in rSeq[i].Trim().Split(" "))
+                {
+                    regexRule += GenRegexRule(Int32.Parse(rNo), rules);
+                }
+                if (i < rSeq.Length - 1)
+                {
+                    regexRule += "|";
+                }
+            }
+
+            regexRule += ")";
+
+            return regexRule;
+        }
+
+        public static string GetRuleVal(string rule)
+        {
+            return rule.Split("\"")[1];
+        }
+
+    }
+}
---
+++ AOC2020/DayPuzzles/Day19Puzzle.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using Util = AOC2020.DayLibs.Day19Lib.D19Util;
+
+namespace AOC2020.DayPuzzles
+{
+    public class Day19Puzzle : IDayPuzzle
+    {
+        public string Day { get; } = "19";
+
+        public void SolvePart1()
+        {
+
+            Dictionary<int, string> rules = Util.GetRules();
+            string[] messages = Util.GetMessages();
+            string regexRule = "^" + Util.GenRegexRule(0, rules) + "$";
+            int matches = 0;
+            Match match;
+            Regex rx = new Regex(regexRule);
+
+            foreach (string msg in messages)
+            {
+                match = rx.Match(msg);
+                if (match.Success)
+                {
+                    matches++;
+                }
+            }
+            Console.WriteLine($"Number of matches to rule 0: {matches}");
+        }
+
+        public void SolvePart2()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SolveTest()
+        {
+            Dictionary<int, string> rules = Util.GetRules(part: "Test");
+            Console.WriteLine($"'{Util.GenRegexRule(0, rules)}'");
+        }
+    }
+}
